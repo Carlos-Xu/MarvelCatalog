58e823e7538b2e567b83b7387b476311c5a1ac16
Carlos Xu <carlos.xu.g@gmail.com>
2021-09-24 11:58:23 +0100

    print explanations on fatalErrors

--- MarvelCatalog/di/di_setup.swift
+++ MarvelCatalog/di/di_setup.swift
@@ -21,9 +21,15 @@ func initializeDIContainer() -> Container {
     
     
     container.register(MarvelRepo.self) { _ in
-        let baseUrl: String = try! Config.value(for: .marvelApiBaseUrl)
-        let privateKey: String = try! Config.value(for: .marvelPrivateApiKey)
-        let publicKey: String = try! Config.value(for: .marvelPublicApiKey)
+        guard let baseUrl: String = try? Config.value(for: .marvelApiBaseUrl) else {
+            fatalError("Base url for Marvel API not configured")
+        }
+        guard let privateKey: String = try? Config.value(for: .marvelPrivateApiKey) else {
+            fatalError("Private key for Marvel API not configured")
+        }
+        guard let publicKey: String = try? Config.value(for: .marvelPublicApiKey) else {
+            fatalError("Public key for Marvel API not configured")
+        }
         
         return MarvelRepoImpl(baseUrl: baseUrl, privateKey: privateKey, publicKey: publicKey)
     }
